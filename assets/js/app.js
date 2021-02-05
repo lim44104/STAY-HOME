0c12e7866fa9b0582f50c7d3d44578a1cf92a186
lim44104 <s10207455@connect.np.edu.sg>
2021-02-05 17:06:19 +0800

    Debug for failed api call in github

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,3 @@
-if (window.location.protocol.indexOf('https') == 0){
-  var el = document.createElement('meta')
-  el.setAttribute('http-equiv', 'Content-Security-Policy')
-  el.setAttribute('content', 'upgrade-insecure-requests')
-  document.head.append(el)
-}
 
 //get all the elements
 const country_name_element = document.querySelector(".country .name");
